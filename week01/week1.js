3076b29fd1139fd4d4431b7c8d1aeb4655796aa9
Thanakritkhamwicha012 <177916817+Thanakritkhamwicha012@users.noreply.github.com>
2025-08-10 23:27:40 +0700

    add week01

---
+++ week01/week1.js
@@ -0,0 +1,13 @@
+//1
+let friends = ["joey", "arm", "lol", "Dana", "Eddy"];
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+}
+//2
+let fruits = ["Apple", "Banana", "Mango"];
+fruits.push("Orange", "Grape");
+console.log(fruits);
+//3
+let numbers = [10, 20, 30, 40, 50];
+numbers.pop();
+console.log(numbers);
